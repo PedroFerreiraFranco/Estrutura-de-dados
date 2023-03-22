03fcb5624804955e4a700d2f7092d927982d20bc
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-03-22 09:19:14 -0300

    Add files via upload
    Fila feita em HTML, JavaScript

---
+++ FilaJavaScript/FilaJavaScript/queue.js
@@ -0,0 +1,41 @@
+class Fila{
+     constructor(maxSize){
+        this.maximo = maxSize;
+        this.itens = [];
+     }
+//---------------------------------------------
+     enqueue(elemento){
+        if(this.itens.length === this.maximo)
+            return false;
+        //this.fim++;
+        //this.itens[this.fim] = elemento;    
+        this.itens.push(elemento);// insere no final
+        return true;    
+     }
+
+     dequeue(){
+        let valor = this.itens.shift();
+        return valor;
+     }
+
+     isEmpty(){
+     /* if(this.itens.length == 0)
+         return true;
+      else
+         return false;  */ 
+       return this.itens.length === 0;
+     }
+
+     toString(){
+       return this.itens.toString();
+     }
+
+     first(){
+         return this.itens[0];
+     }
+
+     last(){
+        return this.itens[this.itens.length-1];
+     }
+    
+}// fim classe 
